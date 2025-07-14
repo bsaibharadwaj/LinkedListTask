147c181dc62c7c22abdf832825380ffae7352cd7
bsaibharadwaj <saibharadwaj926@gmail.com>
2025-07-14 22:30:18 +0530

    Add files via upload

---
+++ SinglyLinkedList.java
@@ -0,0 +1,120 @@
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+public class SinglyLinkedList {
+    Node head;
+
+    
+    public void insertAtBeginning(int data) {
+        Node newNode = new Node(data);
+        newNode.next = head;
+        head = newNode;
+    }
+
+    
+    public void insertAtEnd(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = newNode;
+    }
+
+    
+    public void insertAtPosition(int position, int data) {
+        if (position == 1) {
+            insertAtBeginning(data);
+            return;
+        }
+        Node newNode = new Node(data);
+        Node temp = head;
+        for (int i = 1; i < position - 1 && temp != null; i++) {
+            temp = temp.next;
+        }
+        if (temp == null) {
+            System.out.println("Position out of range");
+            return;
+        }
+        newNode.next = temp.next;
+        temp.next = newNode;
+    }
+
+    
+    public void deleteAtBeginning() {
+        if (head != null) {
+            head = head.next;
+        }
+    }
+
+    
+    public void deleteAtEnd() {
+        if (head == null || head.next == null) {
+            head = null;
+            return;
+        }
+        Node temp = head;
+        while (temp.next.next != null) {
+            temp = temp.next;
+        }
+        temp.next = null;
+    }
+
+   
+    public void deleteAtPosition(int position) {
+        if (position == 1) {
+            deleteAtBeginning();
+            return;
+        }
+        Node temp = head;
+        for (int i = 1; i < position - 1 && temp != null; i++) {
+            temp = temp.next;
+        }
+        if (temp == null || temp.next == null) {
+            System.out.println("Position out of range");
+            return;
+        }
+        temp.next = temp.next.next;
+    }
+
+    
+    public void display() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " → ");
+            temp = temp.next;
+        }
+        System.out.println("NULL");
+    }
+
+    
+    public static void main(String[] args) {
+        SinglyLinkedList list = new SinglyLinkedList();
+
+        list.insertAtBeginning(10);
+        list.insertAtEnd(20);
+        list.insertAtEnd(30);
+        list.insertAtPosition(2, 15);
+        list.display();
+
+        list.deleteAtBeginning();
+        list.display();
+
+        list.deleteAtEnd();
+        list.display();
+
+        list.deleteAtPosition(2);
+        list.display();
+    }
+}
